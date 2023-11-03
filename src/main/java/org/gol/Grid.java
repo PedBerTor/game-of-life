98087550db9a800f64b2750b81f7711730c4593f
PedBerTor <pedrobertorres@gmail.com>
2023-11-03 20:53:11 +0100

    Invert width and height in Grid

--- src/main/java/org/gol/Grid.java
+++ src/main/java/org/gol/Grid.java
@@ -2,21 +2,21 @@ package org.gol;
 
 public class Grid {
 
-    private final int width;
     private final int height;
+    private final int width;
     private final Cell[][] cells;
 
-    public Grid(int width, int height) {
-        this.width = width;
+    public Grid(int height, int width) {
         this.height = height;
-        cells = new Cell[width][height];
+        this.width = width;
+        cells = new Cell[height][width];
         fillGrid();
     }
 
     private void fillGrid() {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                cells[x][y] = new Cell();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                cells[i][j] = new Cell();
             }
         }
     }
