dbb71c9f2c0145ac2a2a4cb5927520ac0303c2fe
PedBerTor <pedrobertorres@gmail.com>
2023-11-08 01:08:53 +0100

    refactor(GOL-4): Add position and neighbor-based state updater to Cell

--- src/main/java/org/gol/Cell.java
+++ src/main/java/org/gol/Cell.java
@@ -2,17 +2,51 @@ package org.gol;
 
 public class Cell {
 
-    private boolean isAlive = false;
+    private final int verticalPosition;
+    private final int horizontalPosition;
 
-    public Cell() {
-        super();
+    private boolean isAlive = false;
+    private int aliveNeighbors = 0;
+
+    public Cell(int verticalPosition, int horizontalPosition) {
+        this.verticalPosition = verticalPosition;
+        this.horizontalPosition = horizontalPosition;
+    }
+
+    public int getVerticalPosition() {
+        return verticalPosition;
+    }
+
+    public int getHorizontalPosition() {
+        return horizontalPosition;
     }
 
     public boolean isAlive() {
         return isAlive;
     }
 
-    public void changeState() {
+    public void switchState() {
         isAlive = !isAlive;
+    }
+
+    public void addAliveNeighbor() {
+        aliveNeighbors++;
+    }
+
+    public void updateStateBasedOnNeighbors() {
+        if (isAliveAndDies()) {
+            isAlive = false;
+        } else if (isDeadAndRevives()) {
+            isAlive = true;
+        }
+        aliveNeighbors = 0;
+    }
+
+    private boolean isAliveAndDies() {
+        return isAlive && (aliveNeighbors < 2 || aliveNeighbors > 3);
+    }
+
+    private boolean isDeadAndRevives() {
+        return !isAlive && aliveNeighbors == 3;
     }
 }
