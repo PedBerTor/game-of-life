c0a907fce64c93d4e10223cf23aebb80ba959d83
Ren <projectren13@gmail.com>
2023-11-15 00:55:06 +0100

    feat(GOL-3): Implement GridDTO class.

--- src/main/java/org/gol/dto/GridDTO.java
+++ src/main/java/org/gol/dto/GridDTO.java
@@ -1,12 +1,64 @@
 package org.gol.dto;
 
-public class GridDTO {
+import org.gol.Grid;
 
-    /* TODO: Implement DTO for Grid
-     * - Private instance variables (see sample.json)
-     * - Empty constructor
-     * - Getters and setters for all instance variables
-     *
-     * IF NEEDED: Static methods fromGrid/toGrid
-     */
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class GridDTO {
+    private int height;
+    private int width;
+    private Set<CellDTO> aliveCells;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Set<CellDTO> getAliveCells() {
+        return aliveCells;
+    }
+
+    public void setAliveCells(Set<CellDTO> aliveCells) {
+        this.aliveCells = aliveCells;
+    }
+
+    public static Grid toGrid(GridDTO gridDTO) {
+        Grid grid = new Grid(gridDTO.getHeight(), gridDTO.getWidth());
+        for (CellDTO cellDTO : gridDTO.getAliveCells()) {
+            grid.getCell(cellDTO.getVerticalPosition(), cellDTO.getHorizontalPosition()).switchState();
+        }
+        return grid;
+    }
+
+    public static GridDTO fromGrid(Grid grid) {
+        GridDTO gridDTO = new GridDTO();
+        gridDTO.setHeight(grid.getHeight());
+        gridDTO.setWidth(grid.getWidth());
+        Set<CellDTO> aliveCells = new LinkedHashSet<>();
+        for (int i = 0; i < grid.getHeight(); i++) {
+            for (int j = 0; j < grid.getWidth(); j++) {
+                if (grid.getCell(i, j).isAlive()) {
+                    CellDTO cellDTO = new CellDTO();
+                    cellDTO.setVerticalPosition(i);
+                    cellDTO.setHorizontalPosition(j);
+                    aliveCells.add(cellDTO);
+                }
+            }
+        }
+        gridDTO.setAliveCells(aliveCells);
+        return gridDTO;
+    }
 }
